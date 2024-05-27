9806e063622abf1f971642eb539a366a768aae3f
Jorrit Strikwerda <jippiejorrit@gmail.com>
2024-05-27 12:06:21 +0200

    Add pipeline .github folder. Add tasks to fetch taaktypes from applications.

--- app/apps/applicaties/admin.py
+++ app/apps/applicaties/admin.py
@@ -1,6 +1,7 @@
 import logging
 
 from apps.applicaties.models import Applicatie
+from apps.applicaties.tasks import fetch_and_save_taaktypes
 from django.contrib import admin, messages
 from django.core.cache import cache
 
@@ -13,6 +14,14 @@ class TaakapplicatieAdmin(admin.ModelAdmin):
         "naam",
         "gebruiker",
     )
+    actions = ["fetch_taaktypes"]
+
+    def fetch_taaktypes(self, request, queryset):
+        for applicatie in queryset:
+            fetch_and_save_taaktypes.delay(applicatie.id)
+        self.message_user(request, "Taaktypes fetching has been started.")
+
+    fetch_taaktypes.short_description = "Fetch and save taaktypes"
 
     def save_model(self, request, obj, form, change):
         if obj.pk:
--- app/apps/taaktypes/admin.py
+++ app/apps/taaktypes/admin.py
@@ -1,1 +1,21 @@
-# Register your models here.
+from apps.applicaties.tasks import fetch_and_save_taaktypes
+from apps.taaktypes.models import Taaktype
+from django.contrib import admin
+
+
+class TaaktypeAdmin(admin.ModelAdmin):
+    list_display = ("taakapplicatie", "taakapplicatie_taaktype_url", "omschrijving")
+    search_fields = ("taakapplicatie__naam", "omschrijving")
+    actions = ["refresh_taaktypes"]
+    list_filter = ("taakapplicatie",)
+
+    def refresh_taaktypes(self, request, queryset):
+        applicatie_ids = queryset.values_list("taakapplicatie_id", flat=True).distinct()
+        for applicatie_id in applicatie_ids:
+            fetch_and_save_taaktypes.delay(applicatie_id)
+        self.message_user(request, "Taaktypes refresh has been started.")
+
+    refresh_taaktypes.short_description = "Refresh selected taaktypes"
+
+
+admin.site.register(Taaktype, TaaktypeAdmin)
