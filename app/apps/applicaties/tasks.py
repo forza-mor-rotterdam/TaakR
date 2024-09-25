74a3704f4b1125d6c39dd75d27c7262ac3be74c3
Maurice Guikema <maurice@mgui.nl>
2024-09-25 10:43:24 +0200

    taaktype uuid removed for taak app data

--- app/apps/applicaties/tasks.py
+++ app/apps/applicaties/tasks.py
@@ -7,9 +7,7 @@ from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 DEFAULT_RETRY_DELAY = 2
-MAX_RETRIES = 6
-
-LOCK_EXPIRE = 5
+MAX_RETRIES = 1
 
 
 class BaseTaskWithRetry(celery.Task):
@@ -20,42 +18,40 @@ class BaseTaskWithRetry(celery.Task):
 
 @shared_task(bind=True, base=BaseTaskWithRetry)
 def fetch_and_save_taaktypes(self, applicatie_id):
-    try:
-        applicatie = Applicatie.objects.get(id=applicatie_id)
-        taaktypes = applicatie.taaktypes_halen()
-        print(f"Taaktypes: {taaktypes}")
-        save_taaktypes(applicatie, taaktypes)
-        logger.info(f"Fetched and saved taaktypes for {applicatie.naam}")
-    except Applicatie.DoesNotExist:
-        logger.error(f"Applicatie with id {applicatie_id} does not exist")
-    except Exception as e:
-        logger.error(f"Error fetching taaktypes for Applicatie id {applicatie_id}: {e}")
+    applicatie = Applicatie.objects.get(id=applicatie_id)
+    taaktypes = applicatie.taaktypes_halen()
+    logger.info(f"Taaktypes: {taaktypes}")
+    save_taaktypes.delay(applicatie, taaktypes)
+    logger.info(f"Fetched and saved taaktypes for {applicatie.naam}")
 
 
+@shared_task(bind=True, base=BaseTaskWithRetry)
 def save_taaktypes(applicatie, taaktypes):
     for taaktype_data in taaktypes:
-        print(f"Taaktype data: {taaktype_data}")
-        uuid = taaktype_data.get("uuid")
-        url = taaktype_data.get("_links", {}).get("self")
+        save_taaktype.delay(applicatie, taaktype_data)
 
-        try:
-            taaktype, created = Taaktype.objects.update_or_create(
-                taakapplicatie_taaktype_url=url,
-                defaults={
-                    "taakapplicatie_taaktype_uuid": uuid,
-                    "taakapplicatie": applicatie,
-                    "actief": taaktype_data.get("actief", True),
-                    "omschrijving": taaktype_data.get("omschrijving", ""),
-                    "toelichting": taaktype_data.get("toelichting", ""),
-                },
-            )
-            if created:
-                logger.info(
-                    f"Created new Taaktype {taaktype.uuid} with for taakapplicatie taaktype with uuid: {uuid}"
-                )
-            else:
-                logger.info(
-                    f"Updated Taaktype {taaktype.uuid} with for taakapplicatie taaktype with uuid: {uuid}"
-                )
-        except Exception as e:
-            logger.error(f"Error saving Taaktype with URL {url}: {e}")
+
+@shared_task(bind=True, base=BaseTaskWithRetry)
+def save_taaktype(applicatie, taaktype_data):
+    logger.info(f"Taaktype data: {taaktype_data}")
+    url = taaktype_data.get("_links", {}).get("self")
+    if isinstance(url, dict):
+        url = url.get("href")
+
+    taaktype, created = Taaktype.objects.update_or_create(
+        taakapplicatie_taaktype_url=url,
+        defaults={
+            "taakapplicatie": applicatie,
+            "actief": taaktype_data.get("actief", True),
+            "omschrijving": taaktype_data.get("omschrijving", ""),
+            "toelichting": taaktype_data.get("toelichting", ""),
+        },
+    )
+    if created:
+        logger.info(
+            f"Created new Taaktype {taaktype.uuid} with for taakapplicatie taaktype with url: {url}"
+        )
+    else:
+        logger.info(
+            f"Updated Taaktype {taaktype.uuid} with for taakapplicatie taaktype with url: {url}"
+        )
